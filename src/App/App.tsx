6823b72882d7b33c6bb81445e7d7d117b0967986
Miroslavetsh <dfgshte@gmail.com>
2021-09-28 13:52:03 +0300

    Setup Typescript, add linter & Container component, refactor architecture

---
+++ src/App/App.tsx
@@ -0,0 +1,7 @@
+import styles from './App.module.scss'
+
+const App = () => {
+  return (<div className={styles.container}>Hello, World</div>)
+}
+
+export default App
---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,7 @@
+import styles from './Container.module.scss'
+
+const Container = () => {
+  return (<div className={styles.container}>Hello, World</div>)
+}
+
+export default Container
